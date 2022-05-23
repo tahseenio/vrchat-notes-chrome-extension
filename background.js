026290dc58357fe1a93cd48f76f9f03123a14fb5
Tahseen <tahseenislam@hotmail.com>
2022-05-24 02:16:24 +1000

    fixed BUG: note div loading twice

--- background.js
+++ background.js
@@ -1,10 +1,23 @@
-function handleUpdated(tabId, changeInfo) {
-  if (changeInfo.url) {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      files: ['reinit.js'],
-    });
-    console.log('Tab: ' + tabId + ' URL changed to ' + changeInfo.url);
-  }
-}
-chrome.tabs.onUpdated.addListener(handleUpdated);
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+  // console.log('Page uses History API and detected tab change:', details.tabId);
+  chrome.tabs.sendMessage(
+    details.tabId,
+    { text: 'are_you_there_content_script?' },
+    function (msg) {
+      msg = msg || {};
+      if (msg.status !== 'yes') {
+        console.log('failed to get status?');
+        chrome.scripting.executeScript({
+          target: { tabId: details.tabId },
+          files: ['content-script.js'],
+        });
+      } else if (msg.status === 'yes') {
+        console.log('status yes');
+        chrome.scripting.executeScript({
+          target: { tabId: details.tabId },
+          files: ['reinit.js'],
+        });
+      }
+    }
+  );
+});
--- content-script.js
+++ content-script.js
@@ -1,7 +1,11 @@
 // TODO: make script dynamically inject once page finished loading instead of using setTimeout
-// TODO: find better way to store user data
 // TODO: create a save backup of localstorage data / restore from backup.
-// BUG: Content and reinit running at same time on first browser load on a profile page
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.text === 'are_you_there_content_script?') {
+    sendResponse({ status: 'yes' });
+  }
+});
 
 const notesDivStyle = `background-color: #242A31;
 margin-top: 16px;
@@ -38,9 +42,6 @@ const main = () => {
 
     const userID = window.location.href.slice(-40);
 
-    // if (localStorage.getItem(userID)) {
-    //   textArea.innerHTML = localStorage.getItem(userID);
-    // }
     let notes = [];
     if (localStorage.getItem('userNotes')) {
       notes = JSON.parse(localStorage.getItem('userNotes'));
@@ -49,7 +50,7 @@ const main = () => {
           textArea.innerHTML = elem.note;
         }
       });
-      console.log('ON MOUNT:', notes);
+      // console.log('ON MOUNT:', notes);
     }
 
     textArea.addEventListener('keyup', (e) => {
@@ -65,7 +66,7 @@ const main = () => {
           });
           notes = [...newArr];
           localStorage.setItem('userNotes', JSON.stringify(notes));
-          console.log('notes edited', notes);
+          // console.log('notes edited', notes);
         }
       });
 
@@ -80,55 +81,11 @@ const main = () => {
         ];
         notes = [...newArr];
         localStorage.setItem('userNotes', JSON.stringify(notes));
-        console.log('notes find', notes);
+        // console.log('notes find', notes);
       }
-
-      // if localstorage of the array exists then getItem the value and set it to the innerHtml of textarea
-      // check if the userID exists in the array and if not exist add in a new object in array
-
-      // if userID exists then update array
-
-      // if (localStorage.getItem('userNotes')) {
-      //   userNotes = JSON.parse(localStorage.getItem('userNotes'));
-      //   textArea.innerHTML = userNotes.filter((elem) => {
-      //     if (elem.userID === userID) return elem.note;
-      //   });
-      // }
-
-      // if (userNotes.find((elem) => elem.userID === userID) === 'undefined') {
-      //   const newArr = [
-      //     ...userNotes,
-      //     {
-      //       userID: userID,
-      //       note: e.target.value,
-      //     },
-      //   ];
-      //   console.log(newArr);
-      //   localStorage.setItem('userNotes', JSON.stringify(newArr));
-      //   userNotes = JSON.parse(localStorage.getItem('userNotes'));
-      // } else {
-      //   // console.log('VALUE EXISTS UPDATE IT');
-      //   const newArr = userNotes.map((elem) => {
-      //     if (elem.userID === userID) {
-      //       return {
-      //         ...elem,
-      //         note: e.target.value,
-      //       };
-      //     } else return elem;
-      //   });
-      //   localStorage.setItem('userNotes', JSON.stringify(newArr));
-      //   userNotes = JSON.parse(localStorage.getItem('userNotes'));
-      //   console.log(JSON.parse(localStorage.getItem('userNotes')));
-      // }
-
-      // localStorage.setItem(window.location.href.slice(-40), e.target.value);
-
-      // get stored array
-      // map through array and update object if userID === elem.userID
-      // else if non existent just push it into array
     });
-  }, 4000);
-  console.log('script ran...');
+  }, 3000);
+  // console.log('script ran...');
 };
 
 main();
