62456e66f2a30aa6ba63b72d9a02745f7e6a044b
Tahseen <tahseenislam@hotmail.com>
2022-05-26 23:17:01 +1000

    added most of functionality for saving and restoring localstorage

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,43 @@
-const saveButton = document.getElementById('save');
-const restoreButton = document.getElementById('restore');
+// TODO: maybe convert to chrome.local.storage if unable to directly obtain access localstorage
 
-// continue this when localstorage converted to chrome storage
-const localStoragetoFile = () => {};
+const getFileText = () => {
+  const [file] = document.getElementById('file').files;
 
-saveButton.addEventListener('click', () => {
-  alert('save button clicked');
+  const reader = new FileReader();
+
+  reader.addEventListener('load', () => {
+    const data = reader.result;
+    const finalData = JSON.parse(data);
+
+    // TODO: insert code to set localstorage as data.json inputted here
+    alert(finalData);
+  });
+
+  if (file) {
+    reader.readAsText(file);
+  }
+};
+
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getFileText();
 });
-restoreButton.addEventListener('click', () => {
-  alert('restore button clicked');
+
+// backup button section
+backup = document.getElementById('backup');
+backup.addEventListener('click', () => {
+  const data = {
+    value: 67,
+  };
+
+  //TODO: need to find a way to access localstorage of vrchat from popup.html
+  // const data = JSON.parse(localStorage.getItem('userNotes'));
+
+  const fileToSave = JSON.stringify(data);
+  const url = 'data:application/json;base64,' + btoa(fileToSave);
+  chrome.downloads.download({
+    url: url,
+    filename: 'userNotes.json',
+  });
 });
